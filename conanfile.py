fc2405b93b0154acb6316ae7e9ba0917cebe7c6f
Jeff DeWall <sr.jilarious@fastmail.net>
2020-12-22 22:41:09 -0700

    Fixing header package export.

--- conanfile.py
+++ conanfile.py
@@ -26,8 +26,8 @@ class ArgunaughtConan(ConanFile):
         # self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="src")
-        self.copy("*.hpp", dst="include", src="src")
+        self.copy("*.h", dst="include/argunaught", src="argunaught/include/argunaught")
+        self.copy("*.hpp", dst="include/argunaught", src="argunaught/include/argunaught")
         self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.dylib*", dst="lib", keep_path=False)
