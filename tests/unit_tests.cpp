b49313c0001bc43448a7672bfbea8991036cae50
Jeff DeWall <sr.jilarious@fastmail.net>
2022-01-04 16:51:45 -0700

    Added unit test passing deque to parse.

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -181,3 +181,39 @@ TEST_CASE( "Test grouped commands", "[groups]" ) {
         REQUIRE(parseResult.command->name == "sub");
     }
 }
+
+TEST_CASE( "Test parsing via deque", "[deque]" ) {
+    int counter = 0;
+    auto argu = argunaught::Parser("Deque parsed")
+        .options({
+                {"delta", "d", "Another global option", 0}
+            })
+        .group("Fancy")
+            .command("sub", "Unit test sub-command", 
+            [&counter] (auto& parseResult) -> int 
+            { 
+                counter = 100;
+                return 0;
+            })
+        .endGroup();
+    
+    SECTION( "Single option without param should work") {
+        std::deque<std::string> args = {"-d"};
+        auto parseResult = argu.parse(args);
+        REQUIRE(!parseResult.hasError());
+        REQUIRE(parseResult.options.size() == 1);
+        REQUIRE(parseResult.options[0].optionName == "delta");
+        REQUIRE(parseResult.options[0].values.size() == 0);
+    }
+
+    SECTION( "Single option with param should work") {
+        std::deque<std::string> args = {"--delta", "sub"};
+        auto parseResult = argu.parse(args);
+        REQUIRE(!parseResult.hasError());
+        REQUIRE(parseResult.options.size() == 1);
+        REQUIRE(parseResult.options[0].optionName == "delta");
+        REQUIRE(parseResult.options[0].values.size() == 0);
+        REQUIRE(parseResult.hasCommand());
+        REQUIRE(parseResult.command->name == "sub");
+    }
+}
